b935cbb28326aacf7180d96e5f266d5e6e2a9a04
Aaron Pang <pang.aaron56@gmail.com>
2015-07-07 23:03:22 -0400

    Initial Commit

---
+++ chatroom/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  chatroom
+//
+//  Created by Aaron Pang on 2015-07-07.
+//  Copyright © 2015 Aaron Pang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ chatroom/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  chatroom
+//
+//  Created by Aaron Pang on 2015-07-07.
+//  Copyright © 2015 Aaron Pang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
