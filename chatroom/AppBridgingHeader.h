49c54b07f463d619f16ca29ee8fbc360b7d5137d
Aaron Pang <pang.aaron56@gmail.com>
2015-07-10 00:48:46 -0400

    Add Facebook login

---
+++ chatroom/AppBridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  AppBridgingHeader.h
+//  chatroom
+//
+//  Created by Aaron Pang on 2015-07-09.
+//  Copyright © 2015 Aaron Pang. All rights reserved.
+//
+
+#ifndef AppBridgingHeader_h
+#define AppBridgingHeader_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* AppBridgingHeader_h */
---
+++ chatroom/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  chatroom
+//
+//  Created by Aaron Pang on 2015-07-09.
+//  Copyright © 2015 Aaron Pang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+  @IBOutlet weak var facebookLoginButton: FBSDKLoginButton!
+
+  override func loadView() {
+    super.loadView()
+    facebookLoginButton.readPermissions = ["public_profile", "email", "user_friends",]
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    if FBSDKAccessToken.currentAccessToken() != nil {
+      performSegueWithIdentifier("loginViewNoAnimationControllerSegue", sender: self)
+    } else {
+    }
+  }
+}
+
+extension LoginViewController : FBSDKLoginButtonDelegate {
+  func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+    guard error == nil else {
+      // Process error
+      return
+    }
+    
+    if result.isCancelled {
+    } else {
+      performSegueWithIdentifier("loginViewControllerSegue", sender: self)
+    }
+  }
+  func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+  }
+}
