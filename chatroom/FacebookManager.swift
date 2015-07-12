8314dec6e194055b3f056a2a0f918fcb9ea2b5f8
Aaron Pang <pang.aaron56@gmail.com>
2015-07-11 20:44:47 -0400

    Add FacebookManager to handle FBSDKGraphRequests

---
+++ chatroom/FacebookManager.swift
@@ -0,0 +1,50 @@
+//
+//  FacebookManager.swift
+//  chatroom
+//
+//  Created by Aaron Pang on 2015-07-10.
+//  Copyright © 2015 Aaron Pang. All rights reserved.
+//
+
+import Foundation
+
+enum FacebookManagerErrorCode: Int {
+  case NoFBSDKAccessToken = 100
+}
+
+
+
+class FacebookManager {
+  static let sharedInstance = FacebookManager()
+  private var name: String?
+  
+  func requestName(completion: (result: String?, error: NSError?) -> ()) {
+    guard FBSDKAccessToken.currentAccessToken() != nil else {
+      let errorCode = FacebookManagerErrorCode.NoFBSDKAccessToken
+      let error = NSError(domain: "FacebookManager", code: errorCode.rawValue, userInfo: ["localizedDescription" : "User is not logged into Facebook on Chatroom."])
+      completion(result: nil, error: error)
+      return
+    }
+    // If the name exists in the singleton, return that. If not, grab it from the user defaults. If it doesn't exist there, make a graph request
+    if name == nil {
+      if let nameFromDefaults = NSUserDefaults.standardUserDefaults().objectForKey("fbName") as? String {
+        name = nameFromDefaults
+      } else {
+        let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+          guard error == nil else {
+            completion(result: nil, error: error)
+            return
+          }
+          if let name = result["name"] as? String {
+            self.name = name
+            NSUserDefaults.standardUserDefaults().setObject(name, forKey: "fbName")
+            completion(result: name, error: nil)
+          }
+        })
+        return
+      }
+    }
+    completion(result:name, error:nil)
+  }
+}
--- chatroom/SecondViewController.swift
+++ chatroom/SecondViewController.swift
@@ -9,16 +9,27 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var profilePicImageView: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  override func loadView() {
+    super.loadView()
+    FacebookManager.sharedInstance.requestName { (result, error) -> () in
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        self.nameLabel.text = result
+      })
     }
+  }
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
 
 
 }
